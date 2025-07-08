cf8439a64d18876d302461def775eb7c3d089583
Abhignya09 <abhignak123@gmail.com>
2025-07-08 09:40:02 +0530

    fixed backend deploy related error

--- server/server.js
+++ server/server.js
@@ -104,7 +104,8 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
